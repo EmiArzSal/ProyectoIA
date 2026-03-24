4ec5eea489fc90dcc972d66c4995e3f8459e1949
Emiliano Arzate <emiarzsal@MacBook-Air-de-Emiliano.local>
2026-03-23 21:45:37 -0600

    chore: update better auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,13 +4,6 @@ import { db } from "@/db"; // your drizzle instance
 import * as schema from "@/db/schema"; // your drizzle schema
  
 export const auth = betterAuth({
-  socialProviders: {
-    microsoft: {
-      clientId: process.env.MICROSOFT_CLIENT_ID as string,
-      clientSecret: process.env.MICROSOFT_CLIENT_SECRET as string,
-      tenantId: process.env.MICROSOFT_TENANT_ID ?? "common",
-    },
-  },
   emailAndPassword: {
     enabled: true,
   },
